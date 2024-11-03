37a31e35f44049d6356c8e0f4494b3833b1c7e9d
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-11-03 21:49:27 +0530

    Connect to db

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
 
 config();
 
@@ -11,6 +14,15 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+prisma
+  .$connect()
+  .then(() => {
+    console.log("Connected to the database.");
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+  });
