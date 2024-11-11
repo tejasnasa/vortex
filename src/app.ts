68eb062d03d902eff9c6d4a147d97a7817685256
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-11-11 11:22:19 +0530

    Deploy(9)

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
 import express from "express";
 import { config } from "dotenv";
+import { PrismaClient } from "@prisma/client";
 import masterRouter from "./routes/masterRouter";
-
-const app = express();
 
 config();
 
+const app = express();
+const prisma = new PrismaClient();
+
 app.use("/v1", masterRouter);
 
-export default app;
+app.use(async (req, res, next) => {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database.");
+    next();
+  } catch (error) {
+    console.error("Database connection error:", error);
+    res.status(500).send("Database connection error");
+  }
+});
+
+export { app, prisma };
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-import { PrismaClient } from "@prisma/client";
-import app from "./app";
+import { app } from "./app";
 
-export const prisma = new PrismaClient();
-
-const port = process.env.PORT;
-
-prisma
-  .$connect()
-  .then(() => {
-    console.log("Connected to the database.");
-
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Failed to connect to the database:", err);
-  });
+export default app;
