25b5aa8d5dd792f7f2d684842d3deeea7ecf0449
Tejas Nasa <tejasnasa2005@gmail.com>
2025-01-14 21:29:18 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,9 @@ app.use(morgan("dev"));
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
